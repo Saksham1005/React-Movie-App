fe76f2f40d879a43ed0bd0c0f5e83ea9e5563778
Saksham1005 <Saksham1005@gmail.com>
2023-02-04 16:10:59 +0530

    Production Build

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,63 +1,61 @@
 // Action types
-export const ADD_MOVIES='ADD_MOVIES'
-export const ADD_FAVOURITES='ADD_FAVOURITES'
-export const REMOVE_FAVOURITES='REMOVE_FAVOURITES'
-export const SHOW_FAVOURITES='SHOW_FAVOURITES'
-export const ADD_MOVIE_TO_LIST='ADD_MOVIE_TO_LIST'
-export const ADD_SEARCH_RESULT='ADD_SEARCH_RESULT'
-
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITES = "ADD_FAVOURITES";
+export const REMOVE_FAVOURITES = "REMOVE_FAVOURITES";
+export const SHOW_FAVOURITES = "SHOW_FAVOURITES";
+export const ADD_MOVIE_TO_LIST = "ADD_MOVIE_TO_LIST";
+export const ADD_SEARCH_RESULT = "ADD_SEARCH_RESULT";
 
 // Action creators
-export function addMovies(movies){
-    return {
-        type:ADD_MOVIES,
-        movies
-    }
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
 }
-export function addFavourites(movie){
-    return {
-        type:ADD_FAVOURITES,
-        movie
-    }
+export function addFavourites(movie) {
+  return {
+    type: ADD_FAVOURITES,
+    movie,
+  };
 }
-export function removeFavourites(movie){
-    return {
-        type:REMOVE_FAVOURITES,
-        movie
-    }
+export function removeFavourites(movie) {
+  return {
+    type: REMOVE_FAVOURITES,
+    movie,
+  };
 }
-export function showFavourites(val){
-    return {
-        type:SHOW_FAVOURITES,
-        val
-    }
+export function showFavourites(val) {
+  return {
+    type: SHOW_FAVOURITES,
+    val,
+  };
 }
 
-
-export function addMovieToList(movie){
-    return {
-        type: ADD_MOVIE_TO_LIST,
-        movie
-    }
+export function addMovieToList(movie) {
+  return {
+    type: ADD_MOVIE_TO_LIST,
+    movie,
+  };
 }
 
 // Async Action
 export function handleMovieSearch(searchText) {
-    return function (dispatch) {
-    const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
+  return function (dispatch) {
+    const url = `https://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
     fetch(url)
-        .then((response) => response.json())
-        .then((movie) => {
-        console.log('movie', movie);
-          // dispatch action to save search results in store
+      .then((response) => response.json())
+      .then((movie) => {
+        console.log("movie", movie);
+        // dispatch action to save search results in store
         dispatch(addMovieSearchResult(movie));
-        });
-    };
+      });
+  };
 }
 
 export function addMovieSearchResult(movie) {
-    return {
+  return {
     type: ADD_SEARCH_RESULT,
     movie,
-    };
+  };
 }
